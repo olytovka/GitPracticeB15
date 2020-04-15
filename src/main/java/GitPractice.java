a714cb6a4950fa8d7dd4931b881b187df66bcf71
olytovka <olhalytovka@gmail.com>
2020-04-15 17:45:38 -0400

    My first commit

---
+++ src/main/java/GitPractice.java
@@ -0,0 +1,11 @@
+public class GitPractice {
+
+    //some new code here
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
